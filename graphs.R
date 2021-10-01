48a683ae9c01efb573f8d2629280c6e1cb6fa158
techieji <drpradhyum2016@outlook.com>
2021-09-30 22:20:00 -0400

    Update graphs.R
    Yes, I'm going to make a bunch of staggered changes.

--- graphs.R
+++ graphs.R
@@ -1,9 +1,13 @@
 library(tidyverse)
 
 # Initialization
-purchases <- read.csv("purchases.csv")   # Remember to change the working dir! Use original purchases.csv!
+purchases <- read.csv("purchases.csv")   # Remember to change working dir! Use original purchases.csv!
 purchases$Source <- sub("^$", "Misc", purchases$Source)
 purchases$Date.Purchased <- do.call(c, lapply(purchases$Date.Purchased, function(x) parse_date(x, "%m/%d/%Y")))   # Untested directly
+
+teaminfo <- read.csv("teaminfo.csv")
+g <- !(teaminfo$Gender %in% c("Male", "Female", "Nonbinary"))
+teaminfo$Gender[g] <- "Other"
 
 # Dollar Comparison
 amt <- purchases %>% group_by(Source) %>% summarize(price = sum(parse_number(Total)))  # Exact Values
@@ -13,8 +17,31 @@ amt %>% ggplot() + geom_col(aes(x = reorder(Source, price), y = price)) + theme(
 purchases %>% ggplot(aes(Date.Purchased)) + geom_density(fill="blue", alpha=0.25)
 
 # Shipping Costs
-purchasesN <- read.csv("purchasesN.csv")   # Remember to copy all shipping values to the Shipping column and delete all taxes!
+purchasesN <- read.csv("purchasesN.csv")   # Remember to copy all shipping values to Shipping column and delete all taxes!
 purchasesN %>% filter(Shipping != "") %>% group_by(Source) %>% summarize(Shipping = sum(parse_number(Shipping))) %>% ggplot(aes(reorder(Source, Shipping), Shipping)) + geom_col() + theme(axis.text.x = element_text(angle = 90)) + xlab("Source") + ylab("Shipping Cost") + ggtitle("A Comparison of Shipping Costs by Seller")
 
 # No. of Orders
 as.data.frame(table(purchasesN$Source)) %>% ggplot(aes(reorder(Var1, Freq), Freq)) + geom_col() + theme(axis.text.x = element_text(angle = 90)) + xlab("Source") + ylab("Frequency") + ggtitle("The Frequency of Order from Various Sellers")
+
+# No. of Orders by Seller
+purchasesN %>% filter(Source %in% c("ANDYMARK", "McMaster", "VEX ROBOTICS")) %>% ggplot(aes(Date.Purchased, fill=Source)) + geom_density(alpha=0.25) + facet_grid(. ~ Source) + ggtitle("When We Bought Items, By Seller")
+
+# Money vs. Date
+purchasesN %>% ggplot(aes(Date.Purchased, weight = parse_number(Total))) + geom_histogram(binwidth=10) + xlab("Purchase Date") + ylab("Amount spent") + ggtitle("How Much We Spent and When")
+
+# Median Price vs. Seller
+purchasesN %>% group_by(Source) %>% summarize(median_price = median(parse_number(Unit.Price))) %>% ggplot(aes(reorder(Source, median_price), median_price)) + geom_col() + theme(axis.text.x = element_text(angle = 90)) + xlab("Source") + ylab("Median Unit Price") + ggtitle("Median Price by Seller")
+
+# Date vs. Quantity
+purchasesN %>% ggplot(aes(Date.Purchased, weight = Quantity.Purchased)) + geom_histogram(binwidth=10) + xlab("Date Purchased") + ylab("Quantity Purchased") + ggtitle("When we Buy a High Number of Items")
+
+# Gender disparity
+teaminfo %>% group_by(Gender) %>% summarize(ct = length(Gender)) %>% ggplot(aes(reorder(Gender, ct), ct)) + geom_col() + xlab("Gender") + ylab("Count") + ggtitle("Gender Disparities in Gryphon Robotics")
+
+# Age disparity
+d <- teaminfo %>% group_by(Grade) %>% summarize(ct = length(Grade))
+d$Grade <- factor(d$Grade, levels = c("Freshman", "Sophomore", "Junior", "Senior"))
+d %>% ggplot(aes(Grade, ct)) + geom_col() + ylab("Count") + ggtitle("Grade Disparities")
+
+# People per sub team
+data.frame(subteams = c("Awards", "Outreach", "Media/Public Relations", "Imagery", "Safety", "Programming", "Electrical", "Mechanical", "Finance", "CAD", "Strat/Scouting") , count = c(2,11,9,13,5,23,22,33,14,10,5)) %>% ggplot(aes(reorder(subteams, count), count)) + geom_col() + theme(axis.text.x = element_text(angle = 90)) + xlab("Subteam") + ggtitle("The Number of People in each Subteam")
