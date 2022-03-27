9d20cda1658b0b25a38340afaba7b0a8b4f0a942
Pradhyum R <drpradhyum2016@outlook.com>
2022-03-27 13:03:40 -0400

    Update lib.py

--- lib.py
+++ lib.py
@@ -7,3 +7,11 @@ class Session:
     
     def get_endpoint(self, endpoint):
         return requests.get("https://www.thebluealliance.com/api/v3" + endpoint, {'X-TBA-Auth-Key': self.key}).json()
+
+def filtermean(f):
+    l = 0
+    s = 0
+    for x in f:
+        l += 1
+        s += x
+    return s / l
